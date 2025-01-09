d63222105716a8e04f0f361060ab189b4c8f6b4a
TaeJin Moon <tj001024@naver.com>
2025-01-10 01:02:34 +0900

    feat : 버스정보 데이터 형식 XML -> DTO 직렬화

--- src/main/java/com/dpod/buschat/businfo/controller/BusApiController.java
+++ src/main/java/com/dpod/buschat/businfo/controller/BusApiController.java
@@ -4,14 +4,16 @@ package com.dpod.buschat.businfo.controller;
 import com.dpod.buschat.businfo.entity.Members;
 import com.dpod.buschat.businfo.service.TestService;
 
+import com.dpod.buschat.businfo.vo.BusStopInfoDto;
+import com.dpod.buschat.businfo.vo.xml.BusStopInfoXml;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.xml.Jaxb2RootElementHttpMessageConverter;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -36,22 +38,26 @@ public class BusApiController {
     }
 
     @GetMapping("/stopinfo/save")
-    public ResponseEntity<String> saveBusStopInfo(){
-        RestClient restClient = RestClient.create();
+    public List<BusStopInfoDto> saveBusStopInfo(){
         String pageNo = "1";
         String numOfRows = "10";
 
-        ResponseEntity<String> busstopInfo = restClient.get()
+        RestClient restClient = RestClient.builder()
+                .messageConverters(List.of(new Jaxb2RootElementHttpMessageConverter()))
+                .build();
+
+        BusStopInfoXml busStopInfoAllXml = restClient.get()
                 .uri("http://openapi.its.ulsan.kr/UlsanAPI/BusStopInfo.xo?pageNo=" +
                         "{pageNo}&numOfRows={numOfRows}&serviceKey={serviceKey}",
                           pageNo,numOfRows,secretkey)
                 .retrieve()
-                .toEntity(String.class);
+                .body(BusStopInfoXml.class);
 
-        return busstopInfo;
+        List<BusStopInfoDto> busStopInfoDtoList = busStopInfoAllXml.getBusStopInfoXmlList().getBusStopInfoDtoList();
+
+        log.info("XML -> DTO : {}", busStopInfoDtoList);
+
+        return busStopInfoDtoList;
     }
-
-
-
 
 }
